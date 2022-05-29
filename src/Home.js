ba6241fda541ac951e9c01d275a6a8d8c5923efe
anish <anishkumarpatro03@gmail.com>
2022-05-29 18:35:15 +0530

    Trending section added

--- src/Home.js
+++ src/Home.js
@@ -261,6 +261,61 @@ const Home=()=> {
         
       </Grid>
     </div>
+    <div className="TrendingSection">
+      <h3>Trending <hr /></h3>
+    </div>
+    <div>
+      <Grid container spacing={0.5} marginTop={1}>
+      <Grid item xs={3} md={2.4}>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+      </CardActionArea>
+    </Card>
+        </Grid>
+        <Grid item xs={3} md={2.4}>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+      </CardActionArea>
+    </Card>
+        </Grid>
+        
+        </Grid> </div>
     </>
   );
 }
